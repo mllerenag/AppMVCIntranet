e7e0c52a9f2f03ece5647fd7c155a8b6a3a8756f
Manuel Monge <manuel.ox29@gmail.com>
2018-08-10 18:46:43 -0500

    Base para Solicitudes
    Base en el Front para trabajarc cambios de Solicitud

---
+++ Celular/Controllers/SolicitudController.cs
@@ -0,0 +1,29 @@
+﻿using Celular.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Celular.Controllers
+{
+    public class SolicitudController : Controller
+    {
+        // GET: Asignar
+        public ActionResult Index()
+        {
+            if (Session["usuario"] != null)
+            {
+                Inicio entidad = new Inicio();
+                return View(entidad);
+            }
+            else
+            {
+                return Redirect("./Login");
+            }
+        }
+
+
+    }
+}
