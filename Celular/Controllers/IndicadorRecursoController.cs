893bff9147c295fae68b1b73ebc0c90a0c52b896
Manuel Monge <manuel.ox29@gmail.com>
2018-09-02 23:59:10 -0500

    Cambios Indicador Recurso
    Cambios en Indicador Recurso

---
+++ Celular/Controllers/IndicadorRecursoController.cs
@@ -0,0 +1,30 @@
+﻿using Celular.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Celular.Controllers
+{
+    public class IndicadorRecursoController : Controller
+    {
+        // GET: Asignar
+        public ActionResult Index()
+        {
+            if (Session["usuario"] != null)
+            {
+                Inicio entidad = new Inicio();
+                entidad.usuario = int.Parse(Session["usuario"].ToString());
+                entidad.nombreusuario = Session["nombreusuario"].ToString();
+
+                return View(entidad);
+            }
+            else
+            {
+                return Redirect("./Login");
+            }
+        }
+    }
+}
